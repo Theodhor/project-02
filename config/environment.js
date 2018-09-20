3bbd4796496cbc15d74d39db2b2433301732a6fc
Theodhor <s.theodhor@yahoo.it>
2018-09-20 09:59:16 +0100

    setting up for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const dbURI = 'mongodb://localhost:27017/project-02';
-const port = 4000;
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project-02';
+const port = process.env.PORT || 4000;
 
 module.exports = { dbURI, port };
